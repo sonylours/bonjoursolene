19c275a8058fc2c82299b628d0715edd53b0abb7
Solene <solene@MacBook-Pro-de-SoleneL-2.local>
2021-03-09 18:46:09 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./src/main.js', './src/scss/main.scss'],
+  watch: true,
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+       test: /\.woff(2)?$/,
+       use: [
+         {
+           loader: 'url-loader',
+           options: {
+             limit: 10000,
+             name: './font/[hash].[ext]',
+             mimetype: 'application/font-woff'
+           }
+         }
+       ]
+     },
+     {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: './img/[hash].[ext]',
+            }
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: '[name].min.css',
+    }),
+  ]
+};
